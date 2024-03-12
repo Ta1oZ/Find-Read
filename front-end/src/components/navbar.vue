353d9ca42ba56685da7fced3cdfdf6b001766d24
Ta1oZ <pond.tnd@gmail.com>
2024-03-12 22:10:08 +0700

    Add modal

--- front-end/src/components/navbar.vue
+++ front-end/src/components/navbar.vue
@@ -1,78 +1,133 @@
 <template>
   <div>
-    <nav class = "bg-zinc-800 w-auto h-14 flex">
-        <!--first nav-items-->
-          <div class="flex items-center justify-start h-full w-full ml-5">
-            <router-link to = "/home"><span class="text-slate-200 hover:text-black flex">
-              <span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-              </svg>
-              </span>FIND-READ</span></router-link>
-              <router-link to="/catagories"><span  class="ml-5 text-slate-200 hover:text-black">Categories</span></router-link>
-              <router-link to="/wishlist"><span  class="ml-5 text-slate-200 hover:text-black">Wishlist</span></router-link>
-              <div v-if="role == 'admin'">
-                <router-link to="/admin" :user="user"><span class="ml-5 text-slate-200 hover:text-black">Admin</span></router-link>
-              </div>
-          </div>
-        <!------------------->
+    <nav class="bg-zinc-800 w-auto h-14 flex">
+      <!--first nav-items-->
+      <div class="flex items-center justify-start h-full w-full ml-5">
+        <router-link to="/home"
+          ><span class="text-slate-200 hover:text-black flex">
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                />
+              </svg> </span
+            >FIND-READ</span
+          ></router-link
+        >
+        <router-link to="/catagories"
+          ><span class="ml-5 text-slate-200 hover:text-black"
+            >Categories</span
+          ></router-link
+        >
+        <router-link to="/wishlist"
+          ><span class="ml-5 text-slate-200 hover:text-black"
+            >Wishlist</span
+          ></router-link
+        >
+        <div v-if="role == 'admin'">
+          <router-link to="/admin" :user="user"
+            ><span class="ml-5 text-slate-200 hover:text-black"
+              >Admin</span
+            ></router-link
+          >
+        </div>
+      </div>
+      <!------------------->
 
-        <!--second-items----->
-          <router-link to="/signup"><button v-if="!user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span  class=" text-slate-200">Sign up</span></button></router-link>
-          <router-link to="/login"><button v-if="!user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span  class=" text-slate-200">Log in</span></button></router-link>
-          <router-link to="/profile"><button v-if="user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span  class=" text-slate-200">My Profile</span></button></router-link>
-          <button v-if="user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span class=" text-slate-200" @click="logout()">Log out</span></button>
+      <!--second-items----->
+      <router-link to="/signup"
+        ><button
+          v-if="!user"
+          class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"
+        >
+          <span class="text-slate-200">Sign up</span>
+        </button></router-link
+      >
+      <router-link to="/login"
+        ><button
+          v-if="!user"
+          class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"
+        >
+          <span class="text-slate-200">Log in</span>
+        </button></router-link
+      >
+      <router-link to="/profile"
+        ><button v-if="user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2">
+          <span class="text-slate-200">My Profile</span>
+        </button></router-link
+      >
+      <button v-if="user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2">
+        <span class="text-slate-200" @click="logout()">Log out</span>
+      </button>
 
-        <!------------------->
+      <!------------------->
     </nav>
   </div>
 </template>
 <script>
-import axios from '@/plugins/axios'
+import axios from "@/plugins/axios";
+import Swal from "sweetalert2";
 export default {
   data() {
-        return {
-            user: null,
-            role: null,
-            ban: null
+    return {
+      user: null,
+      role: null,
+      ban: null,
+    };
+  },
+  mounted() {
+    this.onAuthChange();
+  },
+  methods: {
+    logout() {
+      Swal.fire({
+        title: "Are you sure you want to logout?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonText: "Logout",
+        cancelButtonText: "Cancel",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("role");
+          localStorage.removeItem("ban");
+          this.$emit("auth-change");
+          window.location.reload();
         }
+      });
     },
-    mounted () {
-        this.onAuthChange()
-    },
-    methods: {
-      logout () {
-      localStorage.removeItem('token')
-      localStorage.removeItem('role')
-      localStorage.removeItem('ban')
-      this.$emit('auth-change')
-      window.location.reload()
-    },
-    onAuthChange () {
-      const token = localStorage.getItem('token')
+    onAuthChange() {
+      const token = localStorage.getItem("token");
       if (token) {
-        this.getUser()
+        this.getUser();
       }
     },
-    getUser () {
-      axios.get('/user/me').then(res => {
-        this.user = res.data
-        this.role = res.data.role
-        this.ban = res.data.ban
-        if (this.role == 'admin') {
-          localStorage.setItem('role', 'admin')
+    getUser() {
+      axios.get("/user/me").then((res) => {
+        this.user = res.data;
+        this.role = res.data.role;
+        this.ban = res.data.ban;
+        if (this.role == "admin") {
+          localStorage.setItem("role", "admin");
         }
 
         if (this.ban == 1) {
-          this.logout()
-          alert('You are banned from this website!')
+          this.logout();
+          alert("You are banned from this website!");
         }
-      })
+      });
     },
-  }
-}
-
+  },
+};
 </script>
 <style>
-
 </style>
--- front-end/src/views/login.vue
+++ front-end/src/views/login.vue
@@ -1,72 +1,101 @@
 <template>
-    <div id="app">
-        <div class="flex justify-center items-center h-screen bg-black" id="app">
-        <div class="w-96 p-6 shadow-lg bg-white rounded-xl">
-            <h1 class="block text-center text-3xl font-semibold"><i class="fa-regular fa-user"></i> Log In</h1>
-            <div class="mt-4">
-                <label for="username" class=" block text-base mb-2">Username</label>
-                <input type="text" v-model="username" id="username" placeholder="Enter your username"
-                    class=" border w-full rounded-sm py-2 px-3 focus:outline-none focus:ring-0 focus:border-gray-600">
-            </div>
-            <div class="mt-4">
-                <label for="password" class=" block text-base mb-2">Password</label>
-                <input type="password" v-model="password" id="password" placeholder="Enter your password"
-                    class=" border w-full rounded-sm py-2 px-3 focus:outline-none focus:ring-0 focus:border-gray-600">
-            </div>
-            <div v-if="error">
-                <p class="text-red-500 mt-2"><b>{{ error }}</b></p>
-            </div>
-            <div class="flex justify-between items-center mt-3">
-                <div>
-                    <input type="checkbox">
-                    <label> Remember Me?</label>
-                </div>
-                <div>
-                    <a href="#" class="text-black font-semibold">Forgot Password?</a>
-                </div>
-            </div>
-
-            <div class="mt-5">
-                <button class="w-full bg-black text-white rounded-sm py-2" @click="submit()">Log In</button>
-                <router-link to = "/signup"><span class="w-full text-black rounded-sm py-2 mt-5">Sign Up</span></router-link>
-            </div>
-
+  <div id="app">
+    <div class="flex justify-center items-center h-screen bg-black" id="app">
+      <div class="w-96 p-6 shadow-lg bg-white rounded-xl">
+        <h1 class="block text-center text-3xl font-semibold">
+          <i class="fa-regular fa-user"></i> Log In
+        </h1>
+        <div class="mt-4">
+          <label for="username" class="block text-base mb-2">Username</label>
+          <input
+            type="text"
+            v-model="username"
+            id="username"
+            placeholder="Enter your username"
+            class="border w-full rounded-sm py-2 px-3 focus:outline-none focus:ring-0 focus:border-gray-600"
+          />
         </div>
+        <div class="mt-4">
+          <label for="password" class="block text-base mb-2">Password</label>
+          <input
+            type="password"
+            v-model="password"
+            id="password"
+            placeholder="Enter your password"
+            class="border w-full rounded-sm py-2 px-3 focus:outline-none focus:ring-0 focus:border-gray-600"
+          />
+        </div>
+        <div v-if="error">
+          <p class="text-red-500 mt-2">
+            <b>{{ error }}</b>
+          </p>
+        </div>
+        <div class="flex justify-between items-center mt-3">
+          <div>
+            <input type="checkbox" />
+            <label> Remember Me?</label>
+          </div>
+          <div>
+            <a href="#" class="text-black font-semibold">Forgot Password?</a>
+          </div>
+        </div>
+
+        <div class="mt-5">
+          <button
+            class="w-full bg-black text-white rounded-sm py-2"
+            @click="submit()"
+          >
+            Log In
+          </button>
+          <router-link to="/signup"
+            ><span class="w-full text-black rounded-sm py-2 mt-5"
+              >Sign Up</span
+            ></router-link
+          >
+        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-import axios from '@/plugins/axios';
+import axios from "@/plugins/axios";
+import Swal from "sweetalert2";
 
 export default {
-    data () {
-        return {
-        username: '',
-        password: '',
-        error: ''
-    }
-    },
-    methods: {
-        submit () {
-            const data = {
-            username: this.username,
-            password: this.password
-    }
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    submit() {
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
 
-    axios.post('http://localhost:3001/user/login/', data)
-    .then(res => {
-        alert('Login Success')
-        const token = res.data.token
-        localStorage.setItem('token', token)
-        this.$emit('auth-change')
-        this.$router.push({path: 'home'})
-    })
-    .catch(error => {
-        this.error = error.response.data
-        console.log(error.response.data)
-    })
-}
-}
-}
+      axios
+        .post("http://localhost:3001/user/login/", data)
+        .then((res) => {
+          Swal.fire({
+            icon: "success",
+            title: "Login Success",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          const token = res.data.token;
+          localStorage.setItem("token", token);
+          this.$emit("auth-change");
+          this.$router.push({ path: "home" });
+        })
+        .catch((error) => {
+          this.error = error.response.data;
+          console.log(error.response.data);
+        });
+    },
+  },
+};
 </script>
